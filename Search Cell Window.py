5e4223c15874fc4db78abe9379c446077d0a3088
katawful <45222045+katawful@users.noreply.github.com>
2019-10-04 23:05:05 -0400

    Delete Search Cell Window.py

--- Search Cell Window.py
+++
@@ -1,6 +0,0 @@
-import time
-winTitle="Cell View"
-winClass="tesconstructionset.exe.Wine"
-window.activate(winTitle, switchDesktop=False, matchClass=False)
-time.sleep(0.1)
-mouse.click_relative(80, 50, 1)
